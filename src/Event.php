dbc34174649857b2487c2ae1e815e4bf839c3a01
Tian <leaddeveloper@aevent.com>
2020-11-30 19:31:38 -0500

    Initial

---
+++ src/Event.php
@@ -0,0 +1,147 @@
+<?php
+
+namespace AEvent;
+
+class Event
+{      
+    public $caption = "";
+    /**
+     * breadCrumbs
+     *
+     * @var array
+     */
+    public $breadCrumbs = [];
+    /**
+     * contexts to show (object, array)
+     *
+     * @var array
+     */
+    public $contexts = [];
+    
+    /**
+     * Additional to show (any)
+     *
+     * @var any
+     */
+    public $variables = [];
+    
+    /**
+     * Event Type
+     *
+     * @var string
+     */
+    public $type = Logger::TYPE_DEFAULT;
+
+    public $level = Logger::LEVEL_INFO;
+
+    public $webinarID = null;
+
+    public $fingerprint = null;
+
+    public $tags = [];
+
+    public $user = [
+        "username" => "AEvent",
+        "email" => "AEvent"
+    ];
+
+    public function __construct()
+    {
+    }
+
+    public function setCaption($caption)
+    {
+        $this->caption = $caption;
+        return $this;
+    }
+
+    public function addContext($name, $variable)
+    {
+        $this->contexts[$name] = $variable;
+        return $this;
+    }
+
+    public function setFingerprint($uuid) {
+        $this->fingerprint = $uuid;
+        return $this;
+    }
+
+    public function addData($name, $variable)
+    {
+        $this->variables[$name] = $variable;
+        return $this;
+    }
+
+    public function addTag($name, $tag)
+    {
+        $this->tags[$name] = $tag;
+        return $this;
+    }
+
+    public function setType($type)
+    {
+        $this->type = $type;
+        return $this;
+    }
+
+    public function setLevel($level)
+    {
+        $this->level = $level;
+        return $this;
+    }
+
+    public function setWebinarID($webinarID) {
+        $this->webinarID = $webinarID;
+        return $this;
+    }
+
+    public function setUser($dbname)
+    {
+        $this->user = [
+            "username" => $dbname,
+            "email" => $dbname
+        ];
+        return $this;
+    }
+
+    public function initBreadCrumb($level, $type, $category, $description) {
+        $breadCrumb = [];
+        $breadCrumb["level"] = $level;
+        $breadCrumb["type"] = $type;
+        $breadCrumb["category"] = $category;
+        $breadCrumb["description"] = $description;
+        return  $breadCrumb;
+    }
+
+    public function updateBreadCrumbs()
+    {
+        $this->breadCrumbs = [];
+        $this->breadCrumbs[] = $this->initBreadCrumb(Logger::LEVEL_INFO, \Sentry\Breadcrumb::TYPE_USER, "From", $this->user["username"]);
+        switch ($this->type) {
+            case Logger::TYPE_DEFAULT:
+                $this->breadCrumbs[] = $this->initBreadCrumb(Logger::LEVEL_INFO, \Sentry\Breadcrumb::TYPE_NAVIGATION, "Type", "Info");
+            break;
+            case Logger::TYPE_UPLOAD:
+                $this->breadCrumbs[] = $this->initBreadCrumb(Logger::LEVEL_INFO, \Sentry\Breadcrumb::TYPE_NAVIGATION, "Type", "Upload");
+            break;
+            case Logger::TYPE_WEBINAR:
+                $this->breadCrumbs[] = $this->initBreadCrumb(Logger::LEVEL_INFO, \Sentry\Breadcrumb::TYPE_NAVIGATION, "Type", "Webinar");
+            break;
+            case Logger::TYPE_ALERT:
+                $this->breadCrumbs[] = $this->initBreadCrumb(Logger::LEVEL_INFO, \Sentry\Breadcrumb::TYPE_NAVIGATION, "Type", "Alert");
+            break;
+            default:
+                $this->breadCrumbs[] = $this->initBreadCrumb(Logger::LEVEL_INFO, \Sentry\Breadcrumb::TYPE_NAVIGATION, "Type", $this->type);
+        }
+        if ($this->webinarID) {
+            $this->breadCrumbs[] = $this->initBreadCrumb(Logger::LEVEL_INFO, \Sentry\Breadcrumb::TYPE_DEFAULT, "Webinar", $this->webinarID);
+        }
+        // $this->breadCrumbs[] = $this->initBreadCrumb($this->level, \Sentry\Breadcrumb::TYPE_DEFAULT, "Message", $this->caption);
+        return $this;
+    }
+
+    public function save() {
+        $logger = new Logger();
+        $logger->save($this);
+    }
+}
---
+++ src/Logger.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace AEvent;
+
+class Logger
+{
+    const TYPE_DEFAULT = "default";
+    const TYPE_WEBINAR = "webinar";
+    const TYPE_UPLOAD = "upload";
+    const TYPE_ALERT = "alert";
+
+    const LEVEL_DEBUG = "debug";
+    const LEVEL_INFO = "info";
+    const LEVEL_WARNING = "warning";
+    const LEVEL_ERROR = "error";
+    const LEVEL_FATAL = "fatal";
+
+    public function __construct()
+    {
+    }
+
+    public static function log($fingerprint = null, $caption = "", $user = null, $webinarID = null, $type = null, $level = null, $contexts = null, $variables = null) {
+        $event = new Event();
+        $event->setCaption($caption);
+        if ($user) {
+            $event->setUser($user);
+        }
+        if ($webinarID) {
+            $event->setWebinarID($webinarID);
+        }
+        if ($type) {
+            $event->setType($type);
+        }
+        if ($fingerprint) {
+            $event->setFingerprint($fingerprint);
+        }
+        if ($level) {
+            $event->setLevel($level);
+        }
+        if ($contexts) {
+            foreach ($contexts as $name => $context) {
+                $event->addContext($name, $context);
+            }
+        }
+        if ($variables) {
+            foreach ($variables as $name => $variable) {
+                $event->addData($name, $variable);
+            }
+        }
+        $event->updateBreadCrumbs();
+        return $event->save();
+    }
+
+
+    public function save(Event $event)
+    {
+        \Sentry\withScope(function (\Sentry\State\Scope $scope) use($event) : void {
+            $fingerPrints = [
+                $event->user["username"],
+                $event->type
+            ];
+            foreach ($event->contexts as $name => $variable) {
+                $scope->setContext($name, $variable);
+            }
+            foreach ($event->variables as $name => $variable) {
+                $scope->setExtra($name, $variable);
+            }
+            $scope->setTag('type', $event->type);
+            if ($event->fingerprint) {
+                $scope->setTag('fingerprint', $event->fingerprint);
+                $fingerPrints[] = $event->fingerprint;
+            }
+            if ($event->webinarID) {
+                $scope->setTag('webinarID', $event->webinarID);
+                $fingerPrints[] = $event->webinarID;
+            }
+            $scope->setTag('logger', 'custom');
+            $scope->setUser($event->user);
+            $scope->setFingerprint($fingerPrints);
+            foreach ($event->breadCrumbs as $breadCrumb) {
+                $scope->addBreadcrumb(new \Sentry\Breadcrumb(
+                    $breadCrumb["level"],
+                    $breadCrumb["type"],
+                    $breadCrumb["category"],
+                    $breadCrumb["description"]
+                ));
+            }
+            $scope->setLevel(new \Sentry\Severity($event->level));
+            \Sentry\captureMessage($event->caption);
+        });
+    }
+}
